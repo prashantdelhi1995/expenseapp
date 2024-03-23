cb66dc5907a42286454d8137bbff166c23c76f9c
prashantdelhi1995 <00udemy00@gmail.com>
2024-03-23 18:22:30 +0530

    file added

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,6 +1,7 @@
+require("dotenv").config()
 const {Sequelize, DataTypes}= require("sequelize");
-const sequelize= new Sequelize("expense","root","123456",{
-    host: 'localhost',
+const sequelize= new Sequelize(process.env.DATABASE_SCHEMA, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD,{
+    host: process.env.DATABASE_HOST,
     dialect: 'mysql',
 
 });
